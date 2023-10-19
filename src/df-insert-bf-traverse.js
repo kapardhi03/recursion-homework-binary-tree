af8102ec30d591a76cc8d621645b65f2c5cd169d
kapardhi03 <118623227+kapardhi03@users.noreply.github.com>
2023-10-19 14:50:28 +0530

    Update df-insert-bf-traverse.js

--- src/df-insert-bf-traverse.js
+++ src/df-insert-bf-traverse.js
@@ -6,46 +6,101 @@ const createNode = (value) => {
     }
 }
 
+// const depthFirstInsert = (root, value) => {
+//     if (root.value === value || !value)
+//         return root;
+
+//     // insert on the left side of the tree
+//     if (value < root.value) {
+//         if (root.left) {
+//             return depthFirstInsert(root.left, value);
+//         } else {
+//             root.left = createNode(value);
+//             return root;
+//         }
+//     }
+
+//     // insert on the right side of the tree
+//     if (root.value < value) {
+//         if (root.right) {
+//             return depthFirstInsert(root.right, value);
+//         } else {
+//             root.right = createNode(value);
+//             return root;
+//         }
+//     }
+// }
+
 const depthFirstInsert = (root, value) => {
-    if (root.value === value || !value)
-        return root;
+    if (!root) {
+        return createNode(value);
+    }
 
-    // insert on the left side of the tree
-    if (value < root.value) {
-        if (root.left) {
-            return depthFirstInsert(root.left, value);
-        } else {
-            root.left = createNode(value);
-            return root;
+    let current = root;
+
+    while (true) {
+        if (value === current.value || !value) {
+            break;
+        }
+
+        if (value < current.value) {
+            if (current.left) {
+                current = current.left;
+            } else {
+                current.left = createNode(value);
+                break;
+            }
+        } else if (current.value < value) {
+            if (current.right) {
+                current = current.right;
+            } else {
+                current.right = createNode(value);
+                break;
+            }
         }
     }
 
-    // insert on the right side of the tree
-    if (root.value < value) {
-        if (root.right) {
-            return depthFirstInsert(root.right, value);
-        } else {
-            root.right = createNode(value);
-            return root;
-        }
+    return root;
+};
+
+
+// const breadthFirstTraverse = (root) => {
+//     let collector = [];
+//     let q = [root]
+
+//     while (q.length > 0) {
+//         let first = q.shift();
+//         collector.push(first.value);
+//         if (first.left)
+//             q.push(first.left);
+
+//         if (first.right)
+//             q.push(first.right);
+//     }
+
+//     return collector;
+// };
+const breadthFirstTraverseRecursive = (queue, collector) => {
+    if (queue.length === 0) {
+        return collector;
     }
-}
+
+    const first = queue.shift();
+    collector.push(first.value);
+
+    if (first.left) {
+        queue.push(first.left);
+    }
+
+    if (first.right) {
+        queue.push(first.right);
+    }
+
+    return breadthFirstTraverseRecursive(queue, collector);
+};
 
 const breadthFirstTraverse = (root) => {
-    let collector = [];
-    let q = [root]
-
-    while (q.length > 0) {
-        let first = q.shift();
-        collector.push(first.value);
-        if (first.left)
-            q.push(first.left);
-
-        if (first.right)
-            q.push(first.right);
-    }
-
-    return collector;
+    return breadthFirstTraverseRecursive([root], []);
 };
 
 module.exports = { depthFirstInsert, breadthFirstTraverse, createNode };
